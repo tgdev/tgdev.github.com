13f11acf86b1ff63718a59bf57d15cf0c4307f73
Tom Gillard <tomgillar@gmail.com>
2015-04-07 00:27:20 +1000

    Loading js async to improve page speed and rendering

--- source/js/site-scripts.js
+++ source/js/site-scripts.js
@@ -1,29 +1,32 @@
 // Site javascript file
 
+// Libs
+//= require libs/_jquery-1.11.1-custom.js
+
 // Plugins
 //= require plugins/_console.js
-//= require plugins/_fontfaceobserver.js
+// require plugins/_fontfaceobserver.js
 //= require plugins/_smoothState.js
 
 //= require _namespace.js
 //= require _common.js
 // require src/_fonts.js
 
-(function(w, $) {
+(function($) {
 
 	'use strict';
 
-	if(w.document.documentElement.className.indexOf('fonts-loaded') > -1) {
-		return;
-	}
+	// if(w.document.documentElement.className.indexOf('fonts-loaded') > -1) {
+	// 	return;
+	// }
 
-	var fontA = new w.FontFaceObserver('LeagueGothicReg', {});
+	// var fontA = new w.FontFaceObserver('LeagueGothicReg', {});
 
-	w.Promise
-		.all([fontA.check()])
-		.then(function(){
-			w.document.documentElement.className += ' fonts-loaded';
-		});
+	// w.Promise
+	// 	.all([fontA.check()])
+	// 	.then(function(){
+	// 		w.document.documentElement.className += ' fonts-loaded';
+	// 	});
 
 	$(function() {
 
@@ -34,4 +37,4 @@
 
 	});
 
-})(this, jQuery);
+})(jQuery);
