f4aaefd3e8e88a46f911771b92d11be858e917ca
Tom Gillard <tomgillar@gmail.com>
2015-04-07 01:00:07 +1000

    Applied font lazy loading for faster page rendering

--- source/js/site-scripts.js
+++ source/js/site-scripts.js
@@ -5,35 +5,20 @@
 
 // Plugins
 //= require plugins/_console.js
-// require plugins/_fontfaceobserver.js
 //= require plugins/_smoothState.js
 
 //= require _namespace.js
 //= require _common.js
-// require src/_fonts.js
 
 (function($) {
 
 	'use strict';
-
-	// if(w.document.documentElement.className.indexOf('fonts-loaded') > -1) {
-	// 	return;
-	// }
-
-	// var fontA = new w.FontFaceObserver('LeagueGothicReg', {});
-
-	// w.Promise
-	// 	.all([fontA.check()])
-	// 	.then(function(){
-	// 		w.document.documentElement.className += ' fonts-loaded';
-	// 	});
 
 	$(function() {
 
 		'use strict';
 
 		tg.common.init();
-		// tg.fonts.init();
 
 	});
 
